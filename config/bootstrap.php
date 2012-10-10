0e9f3b63901dc89ae5fde972c83d70593fc8a2a6
Jorge B <jorge@artvisual.net>
2012-10-10 09:38:43 +0200

    filenames changed for cake 2.0

--- config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-Configure::write('MediaPlugin', array(
-	'enabled' => true,
-	'useTokens' => false,
-	'links' => 'hard',
-	'fallback' => 'favicon.ico',
-	'storeOriginal' => false,
-	'store' => true,
-));
--- config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Forward missing media requests to the media serve funciton
- */
-Router::connect(
-	'/:mediaType/*',
-	array('plugin' => 'media', 'controller' => 'media', 'action' => 'serve'),
-	array('mediaType' => '(aud|doc|gen|ico|img|txt|vid)')
-);
